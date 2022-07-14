e827dd1b24cbd6472ce59396fd9cd066c9f29bbb
Chintak4326 <chintakd999@gmail.com>
2022-07-14 18:42:07 +0530

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'ChintakExtensions_SavePrice',
+    __DIR__
+);
